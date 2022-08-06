6595e04dfcd014278b41338cf4a323ea325329da
Cytin7 <99779224+Cytin7@users.noreply.github.com>
2022-08-06 22:29:18 +0800

    Modify

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+将example.config.yaml重命名为config.yaml，并修改包括尖括号部分的相关内容。
 
+将taboo.txt内每行添加检索的违禁词。
--- reply.py
+++ reply.py
@@ -1,6 +1,7 @@
 import os
 import re
 import asyncio
+from types import NoneType
 import botpy
 import requests
 import json
@@ -15,65 +16,91 @@ test_config = read(os.path.join(os.path.dirname(__file__), "config.yaml"))
 _log = logging.get_logger()
 
 
-class MyClient(botpy.Client):
+class BaiduClient(botpy.Client):
     async def on_ready(self):
         # print("Bot is ready")
-        _log.info(f"robot 「{self.robot.name}」 on_ready!")
+        _log.info(f"机器人「{self.robot.name}」正在运行中")
 
     # member message fetch
     async def on_message_create(self, message: Message):
         # _log.info(message.author.avatar)
         msg = message.content
         member = message.member
-
         _log.info(msg)
 
-        # 处理消息，去除符号
-        msg_remove = re.sub(r"(<.*?>)", "", msg)
-        msg_remove = re.sub(r"((&lt;)|(&gt;))+?","",msg_remove)
-        msg_remove = re.sub(r"[\s,\.\?\"\':;!，。？！：；（）\(\)\[\]“”‘’]+?","",msg_remove)
-        # params = {"text": msg}
-        # response = requests.post(request_url, data=params, headers=headers)
-        # if response:
-        #     resp = response.json()
-        #     if resp['conclusionType'] == 2:
-        #         data_list = resp['data']
-        #         text_msg=""
-        #         for data in data_list:
-        #             text_msg = text_msg+'、'+data['msg']
-        #         await message.reply(content=f"您的消息：“{msg}”\n{text_msg[1:]}")
-        #     else:
-        #         await message.reply(content=f"您的消息：“{msg}”")
-        _log.info(msg_remove)
-        length = len(msg_remove)
-        if "4" in member.roles:
-            _log.info("频道主信息")
-            await message.reply(content=f"频道主您好！\n您的消息：“{msg}”\n字符串长度：{length}")
+        # 角色处理
+        if member.roles == NoneType:
+            pass
+        elif "4" in member.roles:
+            # 不处理频道主消息
             return 0
         elif "2" in member.roles:
-            _log.info("管理员信息")
-            await message.reply(content=f"管理员您好！\n您的消息：“{msg}”\n字符串长度：{length}")
+            # 不处理管理员消息
             return 0
         elif "5" in member.roles:
-            _log.info("子管理员信息")
-            await message.reply(content=f"子管理员您好！\n您的消息：“{msg}”\n字符串长度：{length}")
+            # 不处理子管理员消息
             return 0
+
+        # 处理消息，去除符号
+        msg_remove_at = re.sub(r'<[^>]+>', "", msg)  # 去除@信息
+        msg_remove = re.sub(r"[\s\w，。《》？【】！“”‘’：；,\./?!@#$%^&*\(\)\\]+", "", msg_remove_at)
+        msg_remove = re.sub(r"((&lt;)|(&gt;))+", "", msg_remove)
+        msg_no_ascii = re.sub(r"[\s\w]+", "", msg_remove_at)
+
+        _log.info(msg_remove)
+        length = len(msg_remove)
+        for taboo in taboo_list:
+            if taboo in msg_no_ascii:
+                short_msg = msg if len(msg)<=14 else msg[0:7]+"…"+msg[-7:]
+                await message.reply(content=f"您的消息：\n「{short_msg}」\n包含违禁词，请注意语言文明。\n如有误判，请联系频道子管理")
+                return 0
+        if length <= 10:
+            # 不处理10个字以内的消息
+            return 0
+
+        # 百度审核内容
+        params = {"text": msg}
+        response = requests.post(request_url, data=params, headers=headers)
+        if response:
+            resp = response.json()
+            if resp['conclusionType'] == 2:
+                data_list = resp['data']
+                text_msg = ""
+                for data in data_list:
+                    text_msg = text_msg+'、'+data['msg']
+                short_msg = msg if len(msg)<=12 else msg[0:7]+"…"+msg[-7:]
+                await message.reply(content=f"您的消息：\n「{short_msg}」\n被检测出违禁成分，请注意语言文明。\n提示信息：{text_msg[1:]}\n如有误判，请联系频道子管理。")
+            else:
+                await message.reply(content=f"您的消息：\n「{msg}」\n没有问题。")
 
 
 # main program
 if __name__ == "__main__":
-    # text detector API
-    request_url = "https://aip.baidubce.com/rest/2.0/solution/v1/text_censor/v2/user_defined"
+    # 获取违禁词列表
+    taboo_path = os.path.join(os.path.dirname(__file__), "taboo.txt")
+    taboo_file = open(taboo_path, 'r', encoding='utf-8')
+    taboo_list = taboo_file.read().splitlines()
+    # 获取百度NLP工具的API接口token
     baidu_client_id = test_config["baiduapi"]
     baidu_client_secret = test_config["baidusecret"]
     host = f"https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={baidu_client_id}&client_secret={baidu_client_secret}"
     answer = requests.get(host)
     if answer:
+        # 设置百度NLP工具的request格式
+        use_baidu = True
         access_token = answer.json()['access_token']
+        request_url = "https://aip.baidubce.com/rest/2.0/solution/v1/text_censor/v2/user_defined"
         request_url = request_url + "?access_token=" + access_token
         headers = {'content-type': 'application/x-www-form-urlencoded'}
 
-    # bot
-    intents = botpy.Intents(public_guild_messages=True, guild_messages=True)
-    mybot = MyClient(intents=intents)
-    mybot.run(appid=test_config["appid"], token=test_config["token"])
+        # 机器人主体程序
+        intents = botpy.Intents(
+            public_guild_messages=True, guild_messages=True)
+        mybot = BaiduClient(intents=intents)
+        mybot.run(appid=test_config["appid"], token=test_config["token"])
+    else:
+        # 机器人主体程序
+        intents = botpy.Intents(
+            public_guild_messages=True, guild_messages=True)
+        mybot = BaiduClient(intents=intents)
+        mybot.run(appid=test_config["appid"], token=test_config["token"])
